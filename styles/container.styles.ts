051788d01f6f8a0e226f027db5e9dd25e8624593
Tomo-ja <tomonao.hj@gmail.com>
2022-11-25 22:18:05 -0800

    fix: farce background white for dark mode

--- styles/container.styles.ts
+++ styles/container.styles.ts
@@ -2,6 +2,7 @@ import styled from 'styled-components'
 
 const Container = styled.div`
 	display: flex;
+  background-color: white;
   flex-wrap: wrap;
   justify-content: space-between;
   max-width: 1024px;
